6dfbeaf4630ebd3c38d5064fa1fcbc44f82a9539
Jherson Pando <urielth11@gmail.com>
2021-11-11 02:10:21 -0500

    Listado de Receta con video

--- src/main/java/com/cibertec/controller/LoginCont.java
+++ src/main/java/com/cibertec/controller/LoginCont.java
@@ -1,10 +1,14 @@
 package com.cibertec.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
@@ -12,11 +16,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.cibertec.entity.Receta;
+import com.cibertec.repository.RecetaRepo;
+import com.cibertec.service.espec.RecetaServ;
+
 @Controller
 public class LoginCont {
+	
+	@Autowired
+	RecetaServ recetaSer;
 
 	@RequestMapping(value = {"/",""})
 	public String principal(Model model, HttpSession  session) {
+		
+		
+		
 		try {
 			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 			Set<String> roles = authentication.getAuthorities().stream().map(r -> r.getAuthority()).collect(Collectors.toSet());
@@ -46,10 +60,18 @@ public class LoginCont {
 	
 	@RequestMapping(value = {"/inicio","/inicio/"})
 	public String inicio(Model model) {
+		Map<String, Object> salida = new HashMap<>();
+		
 		try {
+			List<Receta> recetas = recetaSer.list();
+			salida.put("recetas", recetas);
+			salida.put("titulo", "Últimas recetas");
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		model.addAllAttributes(salida);
 		return "inicio";
 	}
 }
