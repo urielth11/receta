fb102ffd6db2b05538903c0262595268c5bb8001
Jherson Pando <urielth11@gmail.com>
2021-11-10 20:49:23 -0500

    commit inicial

---
+++ src/main/java/com/cibertec/RecetaApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecetaApplication.class, args);
+	}
+
+}
